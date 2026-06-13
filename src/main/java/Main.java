3ee61cdce9686b9967947c2024ec814bd6241b6d
sarthak1757 <sarthak.raw.17@gmail.com>
2026-06-14 01:09:28 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,11 +4,12 @@ public class Main {
     public static void main(String[] args) throws Exception {
         // TODO: Uncomment the code below to pass the first stage
         System.out.print("$ ");
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String userinput = sc.nextLine();
         // String[] Command = userinput.split(" ");
-        System.out.println(userinput + ": command not found"); 
-           
-        
+        System.out.println(userinput + ": command not found");
+        System.out.print("$ ");
+        sc.close();
+
     }
 }
